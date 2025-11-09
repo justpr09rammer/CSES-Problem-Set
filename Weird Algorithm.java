e78d0ba21aff2f1d6d6927b343463c877fa35dc7
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-11-10 00:50:35 +0400

    Create Weird Algorithm.java

---
+++ Weird Algorithm.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        System.out.print(n + " ");
+        while (n != 1){
+            if (n % 2 == 0){
+                n /= 2;
+            }
+            else n = n * 3 + 1;
+            System.out.print(n + " ");
+        }
+ 
+    }
