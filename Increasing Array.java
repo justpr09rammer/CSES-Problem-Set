49a216878ebd3a28906527f0ddb9e1f62adeae54
nihadgojayev <nihadqocayev152@gmail.com>
2025-11-30 17:56:15 +0400

    updated two files

--- Increasing Array.java
+++ Increasing Array.java
@@ -2,7 +2,6 @@
 import java.util.*;
 import java.util.*;
 
-public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
--- Weird Algorithm.java
+++ Weird Algorithm.java
@@ -4,17 +4,17 @@ import java.util.Collections;
 import java.util.Scanner;
  
 public class Main {
- 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         System.out.print(n + " ");
-        while (n != 1){
-            if (n % 2 == 0){
+        while (n != 1) {
+            if (n % 2 == 0) {
                 n /= 2;
-            }
-            else n = n * 3 + 1;
+            } else n = n * 3 + 1;
             System.out.print(n + " ");
         }
- 
+
     }
+}
