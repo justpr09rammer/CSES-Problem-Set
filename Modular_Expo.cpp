3dce55f1a6352d90574839239df07084ed910687
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2026-03-21 16:43:35 +0400

    Implement two versions of power function with timing
    Added two versions of the power function and measured their execution time.

--- Modular_Expo.cpp
+++ Modular_Expo.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
-
-// f = a ** n;
-long long a_to_the_power_of_n(long long  a, long long n){
-	if (n == 0){
-		return 1;
-	}
-	if (n % 2 == 0){
-		long long half = a_to_the_power_of_n(a, n / 2);
-		return half * half;
-	}
-	else {
-		return a * a_to_the_power_of_n(a, n - 1);
-	}
+long long a_to_the_power_of_n_v1(long long a, long long n){
+    if (n == 0) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        long long half = a_to_the_power_of_n_v1(a, n / 2);
+        return half * half;
+    } else {
+        return a * a_to_the_power_of_n_v1(a, n - 1);
+    }
 }
-
+long long a_to_the_power_of_n_v2(long long a, long long n){
+    if (n == 0) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        return a_to_the_power_of_n_v2(a * a, n / 2);
+    } else {
+        return a * a_to_the_power_of_n_v2(a, n - 1);
+    }
+}
 int main(){
-	int n;
-	cin >> n;
-	while (n --){
-		long long a, b;
-		cin >> a >> b;
-		cout << a_to_the_power_of_n(a, b) << endl;	
-	}
+    long long a, b;
+    cin >> a >> b;
 	
-	return 0;
+    auto start_v1 = chrono::high_resolution_clock::now();
+    cout << "Result for v1: " << a_to_the_power_of_n_v1(a, b) << endl;
+    auto end_v1 = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration_v1 = end_v1 - start_v1;
+    cout << "Time taken by v1: " << duration_v1.count() << " seconds" << endl;
+	
+    auto start_v2 = chrono::high_resolution_clock::now();
+    cout << "Result for v2: " << a_to_the_power_of_n_v2(a, b) << endl;
+    auto end_v2 = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration_v2 = end_v2 - start_v2;
+    cout << "Time taken by v2: " << duration_v2.count() << " seconds" << endl;
+    return 0;
 }
