d727c9e88a61f43ed11e4d68d0c0d8ad580400e2
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2026-03-22 20:16:43 +0400

    Add Euclidean GCD implementation in C++

---
+++ Euclidean_Gcd.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+	if (a == 0){
+		return b;
+	}
+	if (b == 0){
+		return a;
+	}
+	if(a > b){
+		return gcd(a % b, b);
+	}
+	else {
+		return gcd(a, b % a);
+	}
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while (t --){
+		int a, b;
+		cin >> a >> b;
+		cout << "gcd of a and b is " << gcd(a, b) << endl;
+	}
+	return 0;
+}
